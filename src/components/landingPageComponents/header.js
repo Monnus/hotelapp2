653d5e0ce1a1733cf1571e0e689eafc6eca5c208
Monnus <maun2velsket@gmail.com>
2022-12-12 13:54:26 +0200

    doyloy website

--- src/components/landingPageComponents/header.js
+++ src/components/landingPageComponents/header.js
@@ -1,43 +1,120 @@
-import React from 'react'
-import { Box } from '@mui/system'
-import logo from "../../pages/Images/Frame33_logo.png"
-import hotelImage from "../../pages/Images/Frame33_Rectangle2.jpg"
-import HomeIcon from '@mui/icons-material/Home';
-import {Link} from "react-router-dom"
-import AdminPanelSettingsRoundedIcon from '@mui/icons-material/AdminPanelSettingsRounded';
-import BookRoundedIcon from '@mui/icons-material/BookRounded';
-import profileImage from "../../pages/Images/Frame33_Ellipse3.png"
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import app from 'firebaseConfig';
-import { getAuth, signOut } from 'firebase/auth';
+import React from "react";
+import { Box } from "@mui/system";
+import logo from "../../pages/Images/Frame33_logo.png";
+import hotelImage from "../../pages/Images/Frame33_Rectangle2.jpg";
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
+import AdminPanelSettingsRoundedIcon from "@mui/icons-material/AdminPanelSettingsRounded";
+import BookRoundedIcon from "@mui/icons-material/BookRounded";
+import profileImage from "../../pages/Images/Frame33_Ellipse3.png";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import app from "firebaseConfig";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function Header() {
-  const auth=getAuth(app)
+  const auth = getAuth(app);
   return (
-  <>
-    <Box sx={{width:"100%",height:"500px", backgroundColor:"#048BCC", margin:"0 0 70px 0"}}>
-      <nav style={{background:"#5DBBFF",width:"100%"}}>
-        <img src={logo} className="logo" alt=""/>
-        <ul style={{float:"right",display:"inline-block"}}>
-      <Link to="/"><HomeIcon sx={{ fontSize: 40 }} /> <li style={{display:"inline-block"}}>Home</li></Link>
-       <Link to="/AdminsigninPage">  <AdminPanelSettingsRoundedIcon sx={{fontSize:40}}/><li style={{display:"inline-block"}}>admin</li></Link>
-         <Link to="/Bookings">  <BookRoundedIcon sx={{fontSize:40}}/><li style={{display:"inline-block"}}>Bookings</li></Link>
-      <Link Link to="/ "onClick={()=>signOut(auth)}>   <LogoutRoundedIcon sx={{fontSize:40}}/><li style={{display:"inline-block"  }}>logout</li></Link>
-        </ul>
-      </nav>
-      <div style={{backgroundImage:`url(${hotelImage})`,width:"100%",backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition:"center",height:"80%",display:"flex",justifyContent:"center"}}>
-      <h1 style={{color:"white"}}>Welcome to hotel plaza</h1><br></br>
-      <p style={{color:"white"}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent purus ante, tempus eu sollicitudin vel,
-         iaculis quis nunc. Curabitur a ultricies tortor, in accumsan dolor.
-          Etiam sed metus pulvinar, vestibulum arcu eu, finibus tellus.
-           Cras laoreet, massa a cursus egestas, erat est aliquam quam, vitae commodo justo libero nec turpis. 
-           Ut vel arcu massa. Proin rhoncus sagittis efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-           Etiam in risus quis sem pharetra gravida.    </p>
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          zIndex: "1",
+          top: "0",
+          width: "100%",
+          height: "100px",
+          backgroundColor: "#048BCC",
+          margin: "0 0 70px 0",
+        }}
+      >
+        <Link to="/">
+          <img src={logo} className="logo" alt="" />
+        </Link>
+        <nav
+          style={{
+            position: "relative",
+            bottom: "5rem",
+            background: "#5DBBFF",
+            width: "100%",
+          }}
+        >
+          <ul style={{ float: "right", display: "inline-block" }}>
+            <Link to="/">
+              <HomeIcon sx={{ fontSize: 40 }} />{" "}
+              <li style={{ display: "inline-block" }}>Home</li>
+            </Link>
+            <Link to="/AdminsigninPage">
+              {" "}
+              <AdminPanelSettingsRoundedIcon sx={{ fontSize: 40 }} />
+              <li style={{ display: "inline-block" }}>admin</li>
+            </Link>
+            <Link to="/Bookings">
+              {" "}
+              <BookRoundedIcon sx={{ fontSize: 40 }} />
+              <li style={{ display: "inline-block" }}>Bookings</li>
+            </Link>
+            <Link Link to="/ " onClick={() => signOut(auth)}>
+              {" "}
+              <LogoutRoundedIcon sx={{ fontSize: 40 }} />
+              <li style={{ display: "inline-block" }}>logout</li>
+            </Link>
+          </ul>
+        </nav>
+      </Box>
 
+      <div
+        style={{
+          backgroundImage: `url(${hotelImage})`,
+          width: "100%",
+          marginTop: "8rem",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "50rem",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "50rem",
+            background: "rgba(0,0,0,0.5)",
+          }}
+        >
+          <h1
+            style={{
+              color: "#e5e5e5",
+              textAlign: "center",
+              width: "100%",
+              fontWeight: "500",
+              fontSize: "5rem",
+            }}
+          >
+            Welcome to hotel plaza
+          </h1>
+          <br></br>
+          <p
+            style={{
+              display: "block",
+              color: "white",
+              maxWidth: "50rem",
+              fontSize: "1.5rem",
+              lineHeight: "1.7rem",
+              marginTop:"5rem",
+              marginLeft: "1rem",
+            }}
+          >
+            No matter if you want to spend a romantic time with your partner,
+            plan a wellness stay or want to explore a city close up: Our hotel
+            deals will keep all options open for you. Welcome to Hotel plaza.
+          </p>
+        </div>
       </div>
-      <div className='' ><img src={profileImage} style={{borderRadius:"100px"}} className="profileIMGHomePage"/></div>
-    </Box>
-
-        </>
-  )
+      <div className="">
+        <img
+          src={profileImage}
+          style={{ borderRadius: "100px" }}
+          className="profileIMGHomePage"
+        />
+      </div>
+    </>
+  );
 }
--- src/components/landingPageComponents/middle.js
+++ src/components/landingPageComponents/middle.js
@@ -1,180 +1,332 @@
-import React,{useState,useEffect,useContext} from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import { Box } from '@mui/system'
-import Container from '@mui/material/Container';
-import { useNavigate } from 'react-router-dom';
-import { parse } from 'postcss';
-import { hotalData } from 'hotalData';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { UserContext } from 'App';
+import React, { useState, useEffect, useContext } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import SpaIcon from "@mui/icons-material/Spa";
+import MapsHomeWorkIcon from "@mui/icons-material/MapsHomeWork";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
+import { parse } from "postcss";
+import { hotalData } from "hotalData";
+import { useSpring, animated } from '@react-spring/web'
+// import { useInView } from "react-intersection-observer";
+import { InView } from 'react-intersection-observer';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { UserContext } from "App";
+import "./landingpage.css";
+export default function Middle({ hotRoomsObj }) {
+  const theme = createTheme({
+    palette: {
+      bookingscolor: {
+        main: "orange",
+      },
+    },
+  });
+ 
+    const springs = useSpring({
+      from: {
+        opacity: 0,
+        y:"10%",
+        transition:"0"
+      },
+      to: { 
+        opacity: 1,
+        y:"0",
+     transition:"2s ease-in-out"
+    },
+    });
 
 
-export default function Middle({hotRoomsObj}) {
-const theme=createTheme({
-  palette:{
-  bookingscolor:{
-      main:"orange"
-    }
+  const hotalData = useContext(UserContext);
+const containeRooms=document.querySelector(".container-for-rooms");
+
+
+
+  console.log("container-room ",containeRooms);
+  const navigate = useNavigate();
+  function handleBookingsId(id) {
+    navigate("/review", {
+      state: {
+        getID: id,
+      },
+    });
   }
-})
-const hotalData=useContext(UserContext);
-console.log("best rooms",hotalData.bestRooms);
 
-  // console.log(hotRoomsObj);
-const navigate=useNavigate();
-function handleBookingsId(id){
-
- navigate("/review",{state:{
-  getID:id
- }});
-}
-  const newRoom=hotalData.NewRooms.map(data=>{
-    return(
+  const newRoom = hotalData.NewRooms.map((data) => {
+    return (
       <>
-      <Card sx={{ maxWidth: 345 }} key={data.id} style={{position:"relative",margin:"10px",padding:"auto",display:"flex",flexDirection:"column",alignItems:"center"}}>
-      <h2 style={{position:"absolute",right:"10px",top:"0px",textAlign:"center",fontSize:"30px",height:"60px",width:"90px",borderRadius:"100px",color:"white",fontWeight:"800",backgroundImage:"linear-gradient(to top, #4880EC, #019CAD)"}}>New</h2>
-     
-      <CardMedia
-        component="img"
-        height="140"
-        image={data.picture}
-        alt=""
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {data.title}
-        </Typography>
-     
-      </CardContent>
-      <CardActions>
-       <ThemeProvider theme={theme}>
-        <Button color="bookingscolor" variant="contained" 
-        style={{fontWeight:"600"}} size="small" onClick={()=>handleBookingsId(data.id)}>Review</Button>
-       </ThemeProvider>
+        <Card
+          sx={{ maxWidth: 345, minWidth: 345 }}
+          key={data.id}
+          style={{
+            position: "relative",
+            margin: "10px",
+            padding: "auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            
+          }}
+        >
+          <h2
+            style={{
+              position: "absolute",
+              right: "10px",
+              top: "0px",
+              textAlign: "center",
+              fontSize: "30px",
+              height: "60px",
+              width: "90px",
+              borderRadius: "100px",
+              color: "white",
+              fontWeight: "800",
+              backgroundImage: "linear-gradient(to top, #4880EC, #019CAD)",
+              
+            }}
+          >
+            New
+          </h2>
 
-      </CardActions>
-    </Card>
+          <CardMedia component="img" height="140" image={data.picture} alt="" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.title}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <ThemeProvider theme={theme}>
+              <Button
+                color="bookingscolor"
+                variant="contained"
+                style={{ fontWeight: "600" }}
+                size="small"
+                onClick={() => handleBookingsId(data.id)}
+              >
+                Review
+              </Button>
+            </ThemeProvider>
+          </CardActions>
+        </Card>
       </>
-    )
-  })
-const bestSelling=hotalData.bestRoom.map(data=>{
-  return(
-    <>
-    <Card sx={{ maxWidth: 345 }} key={data.id} style={{position:"relative",margin:"10px",padding:"auto",display:"flex",flexDirection:"column",alignItems:"center"}}>
-   
-    <CardMedia
-      component="img"
-      height="140"
-      image={data.picture}
-      alt="Room Images"
-    />
-    {/*    
+    );
+  });
+  const bestSelling = hotalData.bestRoom.map((data) => {
+    return (
+      <>
+        <Card
+          sx={{ maxWidth: 345 }}
+          key={data.id}
+          style={{
+            position: "relative",
+            margin: "10px",
+            padding: "auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <CardMedia
+            component="img"
+            height="140"
+            image={data.picture}
+            alt="Room Images"
+          />
+          {/*    
     <Box
      component="div"
       sx={{ height:"150px",backgroundImage:`url(${data.picture})`,width:"100%",backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition:"center"}}
     > */}
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        {data.title}
-      </Typography>
-   
-    </CardContent>
-    <CardActions>
-     <ThemeProvider theme={theme}>
-      <Button color="bookingscolor" variant="contained" 
-      style={{fontWeight:"600"}} size="small" onClick={()=>handleBookingsId(data.id)}>Review</Button>
-     </ThemeProvider>
-
-    </CardActions>
-  </Card>
-    </>
-  )
-})
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.title}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <ThemeProvider theme={theme}>
+              <Button
+                color="bookingscolor"
+                variant="contained"
+                style={{ fontWeight: "600" }}
+                size="small"
+                onClick={() => handleBookingsId(data.id)}
+              >
+                Review
+              </Button>
+            </ThemeProvider>
+          </CardActions>
+        </Card>
+      </>
+    );
+  });
 
   return (
-  <main>
-<section>
+    <main>
+      <section>
+        <Box
+          sx={{
+            flexGrow: 2,
+            maxWidth: "95%",
+            marginLeft: "2.5%",
+            background: "#048BCC",
+            color: "white",
+            fontWeight: "500",
+            padding: "0.5rem",
+            fontSize: "1.1rem",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item lg={4}>
+              <div style={{ textAlign: "center" }}>
+                <SpaIcon
+                  sx={{
+                    fontSize: "5rem",
+                    color: "black",
+                    border: "2px solid white",
+                    borderRadius: "100%",
+                    width: "6rem",
+                    height: "6rem",
+                  }}
+                />
+                <p>
+                  Ower spa is located where mineral-rich spring water (and
+                  sometimes seawater) is used to give medicinal baths. we have
+                  Spa towns or spa resorts (including hot springs resorts) which
+                  typically offer various health treatments, which are also
+                  known as balneotherapy. The belief in the curative powers of
+                  mineral waters goes back to prehistoric times. Such practices
+                  have been popular worldwide, interested ,the why don't you
+                  join us!{" "}
+                </p>
+              </div>
+            </Grid>
 
-<Box sx={{flexGrow:1, background:"#5DBBFF",color:"white",fontSize:"20px"}}>
-<Grid container spacing={2} >
-        <Grid item lg={4}>
-<div>
-    <h3 className='useCenter'>whats new</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent purus ante, tempus eu sollicitudin vel,
-         iaculis quis nunc. Curabitur a ultricies tortor, in accumsan dolor.
-          Etiam sed metus pulvinar, vestibulum arcu eu, finibus tellus.
-           Cras laoreet, massa a cursus egestas, erat est aliquam quam, vitae commodo justo libero nec turpis. 
-           Ut vel arcu massa. Proin rhoncus sagittis efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-           Etiam in risus quis sem pharetra gravida.</p>
-</div>
+            <Grid item lg={4} className="spacialsBox">
+              <div style={{ textAlign: "center" }}>
+                <MapsHomeWorkIcon
+                  sx={{
+                    fontSize: "5rem",
+                    color: "black",
+                    border: "2px solid white",
+                    borderRadius: "100%",
+                    width: "6rem",
+                    height: "6rem",
+                  }}
+                />
+                <p>
+                  Establishment's that provides paid lodging on a short-term
+                  basis. Facilities provided inside a hotel room may range from
+                  a modest-quality mattress in a small room to large suites with
+                  bigger, higher-quality beds, a dresser, a refrigerator and
+                  other kitchen facilities, upholstered chairs, a flat screen
+                  television, and en-suite bathrooms. Small, lower-priced hotels
+                  may offer only the most basic guest services and facilities.
+                  Larger, higher-priced hotels may provide additional guest
+                  facilities such as a swimming pool, business centre (with
+                  computers, printers, and other office equipment), childcare,
+                  conference and event facilities, tennis or basketball courts,
+                </p>
+              </div>
+            </Grid>
 
-  </Grid>
+            <Grid item lg={4}>
+              <div style={{ textAlign: "center" }}>
+                <DiamondIcon
+                  sx={{
+                    fontSize: "5rem",
+                    color: "black",
+                    border: "2px solid white",
+                    borderRadius: "100%",
+                    width: "6rem",
+                    height: "6rem",
+                  }}
+                />
+                <p>
+                  Feeling as if you want to stay in some Historics classical
+                  hotels,viste or even stay in ower referebrisher hotels that
+                  have maintained their authenticity, sense of place, and
+                  architectural integrity, come and be part of the culture.
+                  participate in new experinces, partake in people, in nature.
+                  feel the times{" "}
+                </p>
+              </div>
+            </Grid>
+          </Grid>
+        </Box>
+      </section>
 
-    <Grid item lg={4} className="spacialsBox">
-    <div>
-    <h3 className='useCenter'>Special deals</h3>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent purus ante, tempus eu sollicitudin vel,
-         iaculis quis nunc. Curabitur a ultricies tortor, in accumsan dolor.
-          Etiam sed metus pulvinar, vestibulum arcu eu, finibus tellus.
-           Cras laoreet, massa a cursus egestas, erat est aliquam quam, vitae commodo justo libero nec turpis. 
-           Ut vel arcu massa. Proin rhoncus sagittis efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-           Etiam in risus quis sem pharetra gravida.    
-    </p>
-</div>
+      <section>
+        <Container maxWidth="lg" sx={{ margin: "0 200px 0 0" }}>
+          <h3
+            style={{
+              margin: "0 0 20px 50%",
+              textAlign: "center",
+              color: "white",
+              width: "200px",
+              height: "60px",
+              background: "#048BCC",
+              borderRadius: "0.1rem",
+              lineHeight: "60px",
+            }}
+          >
+            Best selling rooms
+          </h3>
+          {/* className="container-for-rooms" */}
+          <InView>
+{({inView,ref,entry})=>{
+  console.log("it is in view",inView);
+  
+    return(
+      <animated.div  ref={ref}
+      style={{   
+        background:" #5DBBFF",
+        display: "flex",
+        justifyContent:"space-around",
+        height: "auto",
+        flexWrap: "wrap",
+        ...springs,}}
+        >
+        {newRoom}
+      </animated.div>
+ )
+  
+}}
+          </InView>
+        </Container>
+        <br></br>
 
- 
-  </Grid>
-
-
-<Grid item lg={4}>
-    <div>
-    <h3 className='useCenter'>spacial events</h3>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent purus ante, tempus eu sollicitudin vel,
-         iaculis quis nunc. Curabitur a ultricies tortor, in accumsan dolor.
-          Etiam sed metus pulvinar, vestibulum arcu eu, finibus tellus.
-           Cras laoreet, massa a cursus egestas, erat est aliquam quam, vitae commodo justo libero nec turpis. 
-           Ut vel arcu massa. Proin rhoncus sagittis efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-           Etiam in risus quis sem pharetra gravida.    </p>
- </div>
-</Grid>
-
-        </Grid>
-
-</Box>
-
-</section>
-
-<section>
-
-<Container maxWidth="lg" sx={{margin:"0 200px 0 0"}}>
-  <h3  style={{margin:"0 0 20px 50%",textAlign:"center",color:"white",width:"200px",height:"60px",background:"#048BCC",lineHeight:"60px"}}>Best selling rooms</h3>
-<Box  sx={{background:"#5DBBFF",display:"flex",justifyContent:"space-around",height:"auto", flexWrap:"wrap"}}>
-
-
-
-{newRoom}
-
-
-</Box>
-
-</Container>
-<br></br>
-
-<Container maxWidth="lg" sx={{width:"100%"}}>
-
-<h3 style={{margin:"0 0 20px 50%",textAlign:"center",color:"white",width:"200px",height:"60px",background:"#048BCC",lineHeight:"60px"}}>Rooms</h3>
-<Box sx={{maxHeight:"auto",width:"auto",boxShadow:" 0 0 10px #F5F5F5",display:"flex",flexWrap:"wrap",justifyContent:"space-around"}}>
-{bestSelling}
-</Box>
-</Container>
-</section>
-
-  </main>
-  )
+        <Container maxWidth="lg" sx={{ width: "100%" }}>
+          <h3
+            style={{
+              margin: "0 0 20px 50%",
+              textAlign: "center",
+              color: "white",
+              width: "200px",
+              height: "60px",
+              background: "#048BCC",
+              lineHeight: "60px",
+            }}
+          >
+            Rooms
+          </h3>
+          <Box
+            sx={{
+              maxHeight: "auto",
+              width: "auto",
+              boxShadow: " 0 0 10px #F5F5F5",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+            }}
+          >
+            {bestSelling}
+          </Box>
+        </Container>
+      </section>
+    </main>
+  );
 }
